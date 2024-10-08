8e4ddd2efe4425e80e676071b2dfea9e12d4b300
Gabriel Kyereahene Mensah <s229049206@mandela.ac.za>
2024-10-08 11:49:10 +0200

    Add project files.

---
+++ CORLeaveApproval/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CORLeaveApproval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            ApprovalBase Prossesing = new TeamLeader();
+            ApprovalBase HumanResources = new HR();
+            ApprovalBase manager = new Managers();
+
+
+            Prossesing.SetSuccessor(HumanResources);
+            HumanResources.SetSuccessor(manager);
+
+            string? Result;
+        }
+
+        public abstract class ApprovalBase
+        {
+            protected ApprovalBase _successor;
+            
+            public abstract void Leave(int Days, string? result);
+
+
+        }
+    }
+}
